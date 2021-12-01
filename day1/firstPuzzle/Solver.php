9b483a3db2993ba0602331eb1d1b4cdb32b0fee1
Germain <2016-1044@ensam.eu>
2021-12-01 23:48:16 +0100

    Finish first puzzle of day 1

---
+++ day1/firstPuzzle/Solver.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Day1\FirstPuzzle;
+
+class Solver
+{
+    public static function solve(string $fileName): int
+    {
+        $inputContent = file_get_contents(__DIR__.'/inputs/'.$fileName);
+
+        $depths = explode("\n", $inputContent);
+
+        $depthMeasurementIncreases = 0;
+        $lastDepth = null;
+        foreach($depths as $depth) {
+            if ($lastDepth && $depth > $lastDepth) {
+                $depthMeasurementIncreases ++;
+            }
+            $lastDepth = $depth;
+        }
+
+        return $depthMeasurementIncreases;
+    }
+}
