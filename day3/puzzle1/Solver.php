8186e2992a6571b0473d62552fb025a25029c226
Germain <2016-1044@ensam.eu>
2021-12-04 22:24:56 +0100

    Solve day3/puzzle1

--- day3/puzzle1/Solver.php
+++ day3/puzzle1/Solver.php
@@ -7,9 +7,62 @@ use Utils\Helper;
 
 class Solver
 {
-    public static function solve(string $filePath): int
+    private array $diagnosticReports;
+
+    private int $bitsCount;
+
+    public function __construct(string $filePath)
     {
-        // something
+        $this->diagnosticReports = FileParser::readInputFile($filePath);
+        $this->bitsCount = strlen($this->diagnosticReports[0]);
     }
 
+    public function solve(): int
+    {
+        $gammaRate = $this->computeGammaRate();
+        $epsilonRate =  $this->computeEpsilonRate();
+
+        return $gammaRate * $epsilonRate;
+    }
+
+    public function computeGammaRate(): int
+    {
+        $gammaRate = '';
+        for($bitPosition = 0; $bitPosition < $this->bitsCount; $bitPosition++)
+        {
+            $hasMoreZerosThanOne = $this->hasMoreZerosThanOnes($bitPosition);
+            $gammaRate .= $hasMoreZerosThanOne ? '0' : '1';
+        }
+
+        return bindec($gammaRate);
+    }
+
+    public function computeEpsilonRate(): int
+    {
+        $epsilonRate = '';
+        for($bitPosition = 0; $bitPosition < $this->bitsCount; $bitPosition++)
+        {
+            $hasMoreZerosThanOne = $this->hasMoreZerosThanOnes($bitPosition);
+            $epsilonRate .= $hasMoreZerosThanOne ? '1' : '0';
+        }
+
+        return bindec($epsilonRate);
+    }
+
+    public function hasMoreZerosThanOnes(int $bitPosition): bool
+    {
+        $zeroCount = 0;
+        $oneCount = 0;
+        foreach($this->diagnosticReports as $diagnosticReport)
+        {
+            if (intval(substr($diagnosticReport, $bitPosition, 1) == 0))
+            {
+                $zeroCount++;
+            } else {
+                $oneCount++;
+            }
+        }
+
+        return $zeroCount > $oneCount;
+    }
 }
--- day3/puzzle1/tests/SolverTest.php
+++ day3/puzzle1/tests/SolverTest.php
@@ -2,7 +2,7 @@
 
 namespace Day3\Puzzle1\Tests;
 
-use Day2\Puzzle1\Solver;
+use Day3\Puzzle1\Solver;
 use PHPUnit\Framework\TestCase;
 
 class SolverTest extends TestCase
@@ -10,10 +10,10 @@ class SolverTest extends TestCase
     /**
      * @dataProvider solveProvider
      */
-    public function testSolve(string $inputFileName, int $expectedOutput)
+    public function testSolve(string $inputFilePath, int $expectedOutput)
     {
-        $solution = new Solver();
-        $actualOutput = $solution->solve($inputFileName);
+        $solver = new Solver($inputFilePath);
+        $actualOutput = $solver->solve();
 
         $this->assertEquals($expectedOutput,$actualOutput);
     }
@@ -21,22 +21,22 @@ class SolverTest extends TestCase
     public function solveProvider()
     {
         yield 'Given example should return given answer' => [
-            'file name' => '/day3/inputs/sampleInput.txt',
+            'file path' => '/day3/inputs/sampleInput.txt',
             'expected power consumption' => 198
         ];
         yield 'Given input should return the answer to puzzle' => [
-            'file name' => '/day3/inputs/input.txt',
-            'expected power consumption' => 1602
+            'file path' => '/day3/inputs/input.txt',
+            'expected power consumption' => 3923414
         ];
     }
 
     /**
      * @dataProvider computeGammaRateProvider
      */
-    public function testComputeGammaRate(string $inputFileName, int $expectedOutput)
+    public function testComputeGammaRate(string $inputFilePath, int $expectedOutput)
     {
-        $solution = new Solver();
-        $actualOutput = $solution->computeGammaRate($inputFileName);
+        $solver = new Solver($inputFilePath);
+        $actualOutput = $solver->computeGammaRate();
 
         $this->assertEquals($expectedOutput,$actualOutput);
     }
@@ -44,22 +44,22 @@ class SolverTest extends TestCase
     public function computeGammaRateProvider()
     {
         yield 'Given example should return epsilon rate' => [
-            'file name' => '/day3/inputs/sampleInput.txt',
+            'file path' => '/day3/inputs/sampleInput.txt',
             'expected gamma rate' => 22
         ];
         yield 'Given input should return the correct gamma rate' => [
-            'file name' => '/day3/inputs/input.txt',
-            'expected gamma rate' => 1602
+            'file path' => '/day3/inputs/input.txt',
+            'expected gamma rate' => 2566
         ];
     }
 
     /**
      * @dataProvider computeEpsilonRateProvider
      */
-    public function testComputeEpsilonRate(string $inputFileName, int $expectedOutput)
+    public function testComputeEpsilonRate(string $inputFilePath, int $expectedOutput)
     {
-        $solution = new Solver();
-        $actualOutput = $solution->computeEpsilonRate($inputFileName);
+        $solver = new Solver($inputFilePath);
+        $actualOutput = $solver->computeEpsilonRate();
 
         $this->assertEquals($expectedOutput,$actualOutput);
     }
@@ -67,12 +67,12 @@ class SolverTest extends TestCase
     public function computeEpsilonRateProvider()
     {
         yield 'Given example should return given epsilon rate' => [
-            'file name' => '/day3/inputs/sampleInput.txt',
+            'file path' => '/day3/inputs/sampleInput.txt',
             'expected Epsilon rate' => 9
         ];
         yield 'Given input should return the correct epsilon rate' => [
-            'file name' => '/day3/inputs/input.txt',
-            'expected Epsilon rate' => 1602
+            'file path' => '/day3/inputs/input.txt',
+            'expected Epsilon rate' => 1529
         ];
     }
 }
