541faed90c716ac27318d6dda8ac92fcf07085f2
Germain <2016-1044@ensam.eu>
2021-12-04 23:14:08 +0100

    Solve day3/puzzle2

---
+++ day3/puzzle2/Solver.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Day3\Puzzle2;
+
+use Utils\FileParser;
+use Utils\Helper;
+
+class Solver
+{
+    private array $diagnosticReports;
+
+    private int $bitsCount;
+
+    private string $filePath;
+
+    public function __construct(string $filePath)
+    {
+        $this->diagnosticReports = FileParser::readInputFile($filePath);
+        $this->bitsCount = strlen($this->diagnosticReports[0]);
+        $this->filePath = $filePath;
+    }
+
+    public function solve(): int
+    {
+        $oxygenGeneratorRating = $this->computeOxygenGeneratorRating();
+
+        $this->resetDiagnosticReports();
+
+        $CO2ScrubberRating =  $this->computeCO2ScrubberRating();
+
+        return $oxygenGeneratorRating * $CO2ScrubberRating;
+    }
+
+    public function computeOxygenGeneratorRating(): int // TODO: duplicated with computeCO2ScrubberRating
+    {
+        for($bitPosition = 0; $bitPosition < $this->bitsCount; $bitPosition++)
+        {
+            try {
+                $hasMoreZerosThanOne = $this->hasMoreZerosThanOnes($bitPosition);
+                $this->removeUnwantedDiagnosticReports($hasMoreZerosThanOne ? '0' : '1', $bitPosition);
+            } catch (\LogicException $e) {
+                $this->removeUnwantedDiagnosticReports('1', $bitPosition);
+            }
+
+            if (count($this->diagnosticReports) === 1)
+            {
+                return bindec(current($this->diagnosticReports));
+            }
+        }
+
+        $this->computeOxygenGeneratorRating();
+    }
+
+    public function computeCO2ScrubberRating(): int
+    {
+        for($bitPosition = 0; $bitPosition < $this->bitsCount; $bitPosition++)
+        {
+            try {
+                $hasMoreZerosThanOne = $this->hasMoreZerosThanOnes($bitPosition);
+                $this->removeUnwantedDiagnosticReports($hasMoreZerosThanOne ? '1' : '0', $bitPosition);
+            } catch (\LogicException $e) {
+                $this->removeUnwantedDiagnosticReports('0', $bitPosition);
+            }
+
+            if (count($this->diagnosticReports) === 1)
+            {
+                return bindec(current($this->diagnosticReports));
+            }
+        }
+
+        $this->computeCO2ScrubberRating();
+    }
+
+    public function hasMoreZerosThanOnes(int $bitPosition): bool
+    {
+        $zeroCount = 0;
+        $oneCount = 0;
+        foreach($this->diagnosticReports as $diagnosticReport)
+        {
+            if (intval(substr($diagnosticReport, $bitPosition, 1) == 0))
+            {
+                $zeroCount++;
+            } else {
+                $oneCount++;
+            }
+        }
+
+        if ($zeroCount === $oneCount)
+        {
+            throw new \LogicException(sprintf('Zero count equals one count: %s', $oneCount));
+        }
+        return $zeroCount > $oneCount;
+    }
+
+    private function removeUnwantedDiagnosticReports(string $wantedBit, int $wantedBitPosition): void
+    {
+        $wantedDiagnosticReports = array_filter(
+            $this->diagnosticReports,
+            fn (string $diagnosticReport) => intval(substr($diagnosticReport, $wantedBitPosition, 1) === $wantedBit)
+        );
+
+        $this->setDiagnosticReports($wantedDiagnosticReports);
+    }
+
+    private function setDiagnosticReports(array $diagnosticReports): self
+    {
+        $this->diagnosticReports = $diagnosticReports;
+
+        return $this;
+    }
+
+    private function resetDiagnosticReports(): self
+    {
+        $this->diagnosticReports = FileParser::readInputFile($this->filePath);
+
+        return $this;
+    }
+}
---
+++ day3/puzzle2/tests/SolverTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Day3\Puzzle2\Tests;
+
+use Day3\Puzzle2\Solver;
+use PHPUnit\Framework\TestCase;
+
+class SolverTest extends TestCase
+{
+    /**
+     * @dataProvider solveProvider
+     */
+    public function testSolve(string $inputFilePath, int $expectedOutput)
+    {
+        $solver = new Solver($inputFilePath);
+        $actualOutput = $solver->solve();
+
+        $this->assertEquals($expectedOutput,$actualOutput);
+    }
+
+    public function solveProvider()
+    {
+        yield 'Given example should return given answer' => [
+            'file path' => '/day3/inputs/sampleInput.txt',
+            'expected power consumption' => 230
+        ];
+        yield 'Given input should return the answer to puzzle' => [
+            'file path' => '/day3/inputs/input.txt',
+            'expected life support rating' => 5852595
+        ];
+    }
+
+    /**
+     * @dataProvider computeOxygenGeneratorRatingProvider
+     */
+    public function testComputeOxygenGeneratorRating(string $inputFilePath, int $expectedOutput)
+    {
+        $solver = new Solver($inputFilePath);
+        $actualOutput = $solver->computeOxygenGeneratorRating();
+
+        $this->assertEquals($expectedOutput,$actualOutput);
+    }
+
+    public function computeOxygenGeneratorRatingProvider()
+    {
+        yield 'Given example should return oxygen generator rating' => [
+            'file path' => '/day3/inputs/sampleInput.txt',
+            'expected oxygen generator rating' => 23
+        ];
+        yield 'Given input should return the correct oxygen generator rating' => [
+            'file path' => '/day3/inputs/input.txt',
+            'expected oxygen generator rating' => 2919
+        ];
+    }
+
+    /**
+     * @dataProvider computeCO2ScrubberRatingProvider
+     */
+    public function testComputeCO2ScrubberRating(string $inputFilePath, int $expectedOutput)
+    {
+        $solver = new Solver($inputFilePath);
+        $actualOutput = $solver->computeCO2ScrubberRating();
+
+        $this->assertEquals($expectedOutput,$actualOutput);
+    }
+
+    public function computeCO2ScrubberRatingProvider()
+    {
+        yield 'Given example should return given CO2 scrubber rating' => [
+            'file path' => '/day3/inputs/sampleInput.txt',
+            'expected CO2 scrubber rating' => 10
+        ];
+        yield 'Given input should return the correct CO2 scrubber rating' => [
+            'file path' => '/day3/inputs/input.txt',
+            'expected CO2 scrubber rating' => 2005
+        ];
+    }
+}
